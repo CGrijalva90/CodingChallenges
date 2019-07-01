ce5e4430bac246aad5665cddedb545735532692e
Carlos Grijalva <grijalva.carlos@gmail.com>
2019-07-01 18:19:04 -0400

    Jewels and stones

--- flippingAnImage.js
+++ flippingAnImage.js
@@ -4,9 +4,7 @@
 
 // To invert an image means that each 0 is replaced by 1, and each 1 is replaced by 0. For example, inverting [0, 1, 1] results in [1, 0, 0].
 
-const flipAndInvertImage = A => {
-	return A.map(arr => arr.reverse()).map(arr => arr.map(el => (el ? 0 : 1)));
-};
+const flipAndInvertImage = A => A.map(arr => arr.reverse()).map(arr => arr.map(el => (el ? 0 : 1)));
 
 const image = [[1, 1, 0], [1, 0, 1], [0, 0, 0]];
 
