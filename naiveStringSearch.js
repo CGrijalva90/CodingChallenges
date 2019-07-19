0f64205e7e108b7190a2daf3663c843244eeadbf
Carlos Grijalva <grijalva.carlos@gmail.com>
2019-07-19 12:27:03 -0400

    Refactor

--- naiveStringSearch.js
+++ naiveStringSearch.js
@@ -2,14 +2,8 @@ const searchString = (longStr, shortStr) => {
 	let count = 0
 	for (let i = 0; i < longStr.length; i++) {
 		for (let j = 0; j < shortStr.length; j++) {
-			console.log(shortStr[j], longStr[i+j])
-			if (shortStr[j] !== longStr[i + j]) {
-				break;
-			}
-			if (j === shortStr.length - 1) {
-				console.log(count)
-				count++;
-			}
+			if (shortStr[j] !== longStr[i + j]) break;
+			if (j === shortStr.length - 1) count++;
 		}
 	}
 	return count;
