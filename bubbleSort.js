e5fe39265e54cd37bd975b65fdc6cbfbd4056821
Carlos Grijalva <grijalva.carlos@gmail.com>
2019-07-22 16:49:51 -0400

    Optimize bubble sort and add twoSum

--- bubbleSort.js
+++ bubbleSort.js
@@ -1,15 +1,18 @@
 // implement Bubble Sort
 
-const bubbleSort = (arr) => {
-	for (let i = arr.length - 1; i > 0; i--) {
-		for (let j = 0; j < i - 1; j++) {
+function bubbleSort(arr) {
+	let noSwaps;
+	for (let i = arr.length - 1; i >= 0; i--) {
+		noSwaps = true;
+		for (let j = 0; j <= i - 1; j++) {
+			let temp = arr[j]
 			if (arr[j] > arr[j + 1]) {
-				let temp = arr[j]
 				arr[j] = arr[j + 1]
 				arr[j + 1] = temp
+				noSwaps = false
 			}
 		}
-
+		if (noSwaps) break;
 	}
 	return arr
 }
