e918b899821db5d501b02d6beff3528cd63bf9fc
Carlos Grijalva <grijalva.carlos@gmail.com>
2019-08-01 14:03:52 -0400

    Add quickSort description

--- quickSort.js
+++ quickSort.js
@@ -1,3 +1,5 @@
+// Like Merge Sort, QuickSort is a Divide and Conquer algorithm. It picks an element as pivot and partitions the given array around the picked pivot. There are many different versions of quickSort that pick pivot in different ways
+
 const pivot = (arr, start=0, end=arr.length+1) => {
   const swap = (array, i, j) => {
     var temp = array[i];
